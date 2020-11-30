118ef3ca613fc1b913ddd5fad0a8d874aeb7c1aa
Marty Warfel <marthawarfel@gmail.com>
2020-11-30 02:57:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,22 +1,22 @@
 def key_for_min_value(name_hash)
     low_val = 0 
     low_key = nil
+    
     name_hash.empty? do |key, value|
       if value < smallest_value
         smallest_value = value
       end
     end
       
-    smallest_value = name_hash.first[1]
+    #smallest_value = name_hash.first[1]
       name_hash.each do |key, value|
-      if value < smallest_value
-          smallest_value = value
+      if low_val == 0 || value < low_val
+          low_val = value
+          low_key = key
           end
         end
+low_key
 
-name_hash.each do |key, value|
-  if value == smallest_value
-    return key
 end
 
 end
